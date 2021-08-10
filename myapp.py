6986a394aba8257137c9fcff6eee78c83d18ea3c
Piyush Kumar Sah <64089696+piyushkumargithub@users.noreply.github.com>
2021-08-10 14:31:54 +0530

    Add files via upload

--- myapp.py
+++ myapp.py
@@ -3,8 +3,9 @@ import streamlit as st
 import pandas as pd 
 import numpy as np 
 import sklearn
-import seaborn as sbn
+
 import matplotlib.pyplot as plt
+import Plot
 
 @st.cache(persist=True)
 
@@ -32,42 +33,66 @@ options=["Hyper Cars",
 
 
 def main():
+    
+    
     st.title("SIMPLE EDA")
     st.sidebar.title("It all begins here")
     st.markdown("<h3> WELCOME </h3>",unsafe_allow_html=True)
 
+    #select to choose from available csv files
     selectbox=st.sidebar.selectbox(label="choose file to play around with",options=options)
     st.sidebar.markdown("OR")
+    
+    #if user wants to upload their file
     file=st.sidebar.file_uploader(label="upload your file here")
     
     
+    #to load dataset
     if file==None:    
         data,target=load_data(selectbox)
         st.subheader((selectbox+" dataset").upper())
     elif file!= None:
         data,target=load_own_data(file)
-
-
     
-    rows=st.sidebar.slider(label="rows",min_value=1,max_value=min(100,len(data)))
-    
+    #choose no of rows or entries to show from uploaded dataset
+    rows=st.sidebar.slider(label="rows",min_value=1,max_value=min(100,len(data)))  
     st.dataframe(data.head(rows))
     
     
-    st.sidebar.title("Plot Scatterplot between 2 features ")
-    X_axis=st.sidebar.selectbox("For x axis",options=data.columns.tolist())
-    Y_axis=st.sidebar.selectbox("For y axis",options=data.columns.tolist())
-    if st.sidebar.button("Show Plot"): 
-        if  X_axis!=Y_axis:
-            
-            fig,ax=plt.subplots()
-            ax=sbn.scatterplot(data=data,x=X_axis,y=Y_axis)
-            plt.xticks(rotation="vertical",fontsize=5)
-            st.pyplot(fig)
+    
+    #list of plot available to choose from
+    plotoptions=["Scatterplot","Swarmplot","Lineplot","Heatmap","Barplot"]
+    plotoption=st.sidebar.selectbox(label="choose type of plot",options=plotoptions)
+    
+    
+    if plotoption=="Scatterplot":
+        st.sidebar.title("Plot Scatterplot between 2 features ")
+        X_axis=st.sidebar.selectbox("For x axis",options=data.columns.tolist())
+        Y_axis=st.sidebar.selectbox("For y axis",options=data.columns.tolist())
+        if st.sidebar.button("Show Plot"):
+            Plot.createScatter(data,X_axis,Y_axis)
+
+    elif plotoption=="Swarmplot":
+        pass
+        #part still under construction
+    
+    elif plotoption=="Lineplot":
+        pass
+
+    elif plotoption=="Heatmap":
+        pass
+
+    elif plotoption=="Barplot":
+        pass
+
+
+
+
+    
+    
 
         
-        elif X_axis==Y_axis:
-            st.markdown("## You have choosen same feature for both axis")
+   
         
       
         
